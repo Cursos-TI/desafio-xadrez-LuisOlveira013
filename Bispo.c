115c365835995195183f6e9e75988e849e0926c6
LuisOlveira013 <lo28308@gmail.com>
2025-03-14 01:49:54 +0000

    movimento das peças

---
+++ Bispo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int bispo = 1;
+
+do{
+
+   printf("Cima, direita\n", bispo);
+   bispo++;
+
+} while (bispo <= 5);
+
+return 0;
+
+
+}
---
+++ Rainha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+for (int rainha = 1; rainha <= 8; rainha++)
+{
+    printf("Eaquerda\n", rainha);
+}
+
+return 0;
+    
+}
