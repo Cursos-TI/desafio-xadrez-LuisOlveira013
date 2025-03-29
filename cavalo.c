fd12ce5a582d7c3d8c590d34b157454b9a4ec133
LuisOlveira013 <lo28308@gmail.com>
2025-03-29 03:46:28 +0000

    movimento do cavalo

---
+++ cavalo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+char cavalo = 1;
+ while (cavalo--)
+ {
+    for (int i = 0; i < 2; i++){
+      printf("Baixo\n");
+    }
+    printf("\nEsquerda\n");
+ }
+ 
+
+return 0;
+
+}
